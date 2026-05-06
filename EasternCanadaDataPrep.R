5790e4822ea76b9f7f30384273e92a1c2d8b10ea
Shirin Varkouhi <145788851+shirinvark@users.noreply.github.com>
2026-05-06 10:29:00 -0400

    yyy

--- EasternCanadaDataPrep.R
+++ EasternCanadaDataPrep.R
@@ -250,8 +250,7 @@ buildPlanningGrid <- function(sim) {
   sim$PlanningGrid_250m <- terra::rast(
     ext = terra::ext(study_v),
     resolution = 250,
-    crs = terra::crs(study_v),
-    vals = 1
+    crs = terra::crs(study_v)
   )
   
   planning <- sim$PlanningGrid_250m
